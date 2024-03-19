3aa7332411666632e658f7a48271c9fbf032a843
Xavier Baudhuin <xabaudhu@bess-f3r5s6.clusters.42paris.fr>
2024-03-19 15:09:39 +0100

    early .h

---
+++ include/cub3d.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xabaudhu <xabaudhu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/19 15:03:50 by xabaudhu          #+#    #+#             */
+/*   Updated: 2024/03/19 15:09:25 by xabaudhu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "libft.h"
+# include "ft_printf.h"
+# include "get_next_line.h"
+# include <math.h>
+# include "X11/keysym.h"
+# include "X11/keysymdef.h"
+# include <unistd.h>
+# include <X11/X.h>
+
+# define WIDTH 1920.0
+# define HEIGHT 1080.0
+
+#endif
