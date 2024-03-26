d23c71c0d0f40d94455b700ddf08c70a8af00166
Leo Doyen--Clement <ldoyen--@bess-f3r5s7.clusters.42paris.fr>
2024-03-26 16:55:26 +0100

    add draw functions, create t_timg, some define in header

---
+++ src/draw/draw_line_on_img.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_line_on_img.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldoyen-- <ldoyen--@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/26 16:20:53 by ldoyen--          #+#    #+#             */
+/*   Updated: 2024/03/26 16:20:54 by ldoyen--         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	draw_line_on_img(t_img img, int start_x, int start_y, int end_x,
+	int end_y, int color)
+{
+	double	delta_x;
+	double	delta_y;
+	double	pixel_x;
+	double	pixel_y;
+	int		pixels;
+
+	pixel_x = start_x;
+	pixel_y = start_y;
+	delta_x = end_x - start_x;
+	delta_y = end_y - start_y;
+	pixels = sqrt((delta_x * delta_x) + (delta_y * delta_y));
+	delta_x /= pixels;
+	delta_y /= pixels;
+	while (pixels != 0)
+	{
+		put_pixel_on_img(img, pixel_x, pixel_y, color);
+		pixel_x += delta_x;
+		pixel_y += delta_y;
+		--pixels;
+	}
+}
