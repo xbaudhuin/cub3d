d8c6ab860f6d286f1465be7a5c0dd7cb7ae23d0e
Xavier Baudhuin <xabaudhu@bess-f2r1s11.clusters.42paris.fr>
2024-04-13 14:49:17 +0200

    color minimap

--- src/minimap/minimap_utils.c
+++ src/minimap/minimap_utils.c
@@ -6,7 +6,7 @@
 /*   By: xabaudhu <xabaudhu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 18:51:18 by xabaudhu          #+#    #+#             */
-/*   Updated: 2024/04/11 18:52:15 by xabaudhu         ###   ########.fr       */
+/*   Updated: 2024/04/13 14:39:33 by xabaudhu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,14 @@ int	is_player(t_data_exec *data, int x, int y)
 int	get_color_for_block(const char c)
 {
 	if (c == '\0' || c == '1' || c == ' ')
-		return (0x0018f2);
+		return (0x4E4D4D);
 	if (c == 'p')
-		return (0x921e12);
+		return (0xDE1111);
 	if (c == '0')
 		return (0xbbbbbd);
 	if (c >= '2' && c < '7')
-		return (0x144803);
+		return (0xCC00CC);
 	if (c == '7')
-		return (0x35bd07);
+		return (0xFF66B2);
 	return (0);
 }
